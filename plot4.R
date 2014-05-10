78827b148cef316953491628228902fc797e25bd
sjindalg <sjindalg@adobe.com>
2014-05-10 22:05:42 +0530

    adding graphs and source

--- plot4.R
+++ plot4.R
@@ -25,7 +25,7 @@ with(power,plot(DateTime, Global_active_power,xlab="datetime",ylab="Voltage",typ
 with(power,plot(DateTime, Sub_metering_1,xlab="",ylab="Energy Sub Metering",type="l"))
 with(power,lines(DateTime, Sub_metering_2,col="red"))
 with(power,lines(DateTime, Sub_metering_3,col="blue"))
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = c("solid"), pch = c(" "," ", " "),col = c("black","red", "blue"),lwd = c(1,1))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = c("solid"), pch = c(" "," ", " "),col = c("black","red", "blue"),lwd = c(1,1),bty = "n")
 with(power,plot(DateTime, Global_reactive_power,xlab="datetime",ylab="Global Reactive Power",type="l"))
 
 #save tyo file
